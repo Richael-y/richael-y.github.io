10a510a1117f901cd92aa46d05477bf2f2453fab
Rzy <rzy@tju.edu.cn>
2021-09-11 12:42:38 +0800

    Site updated: 2021-09-11 12:42:38

--- js/langselect.js
+++ js/langselect.js
@@ -1,16 +1,1 @@
-(function() {
-  function changeLang() {
-    var selectLang = this.value;
-    var canonical = this.dataset.canonical;
-    var url = canonical.split('/')[0];
-    var lang = url === 'index.html' ? '/' : (url === 'en' ? '/' : url);
-    var index = canonical.indexOf('/') === -1 ? 0 : canonical.indexOf('/');
-    var path = canonical.substring(index);
-    path = selectLang + (path === 'index.html' ? '/index.html' : path);
-    location.href = (location.origin + '/' + path).replace('index.html', '');
-  }
-
-  if (document.getElementById('lang-select')) {
-    document.getElementById('lang-select').addEventListener('change', changeLang);
-  }
-}());
+document.getElementById("lang-select")&&document.getElementById("lang-select").addEventListener("change",(function(){var e=this.value,n=this.dataset.canonical,t=(n.split("/")[0],-1===n.indexOf("/")?0:n.indexOf("/")),i=n.substring(t);i=e+("index.html"===i?"/index.html":i),location.href=(location.origin+"/"+i).replace("index.html","")}));
